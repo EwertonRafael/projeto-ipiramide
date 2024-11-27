41d0668253cc25cbf31cef6adca50dba9b760c64
Ewerton Rafael <ton_rafael@hotmail.com>
2024-11-27 05:54:02 -0300

    feat: componente sidebar mais o estilo

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <a href="#">Gerencial</a>
+      <a href="#">Autorizações</a>
+    </div>
+  )
+}
