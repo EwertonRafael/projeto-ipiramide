cdbcdb48df509cb211f48e08d59ff43ffcb49636
Ewerton Rafael <ton_rafael@hotmail.com>
2024-11-29 20:05:09 -0300

    refactor: ajustando o login com o componente input e adicionando estado

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,26 +1,62 @@
-import logoMarca from '../../assets/img/LogoMarca.png'
-import React from 'react'
-import './style.css'
-import { Button } from '../../components/button/Button'
+import logoMarca from '../../assets/img/LogoMarca.png';
+import React from 'react';
+import './style.css';
+import { Button } from '../../components/button/Button';
+import { Input } from '../../components/input/Input';
 
 export const Login = () => {
+  const [form, setForm] = React.useState({
+    user: '',
+    password: '',
+  });
+
+  const handleForm = (event) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    try {
+      console.log('Form enviado:', form);
+    } catch (error) {
+      console.error('Erro ao enviar o formulário:', error);
+    }
+  };
+
   return (
     <>
       <section className="login">
         <div>
           <img src={logoMarca} alt="logo" />
-          <h1 className='title'>iPIRAMIDE</h1>
+          <h1 className="title">iPIRAMIDE</h1>
           <p>ACESSAR O SISTEMA</p>
-
         </div>
-        <form action="" >
-          <label htmlFor="user">Nome</label>
-          <input type="text" placeholder="Slecione o usuário"  id='user'/>
-          <label htmlFor="password">Senha</label>
-          <input type="password" placeholder="Selecione sua senha" id='password'/>
-          <Button value="Entrar" />
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="Selecione o usuário"
+            id="user"
+            label="Nome"
+            name="user"
+            value={form.user}
+            onChange={handleForm}
+          />
+          <Input
+            type="password"
+            placeholder="Selecione sua senha"
+            id="password"
+            label="Senha"
+            name="password"
+            value={form.password}
+            onChange={handleForm}
+          />
+          <Button value="Entrar" type="submit" />
         </form>
       </section>
     </>
-  )
-}
+  );
+};
